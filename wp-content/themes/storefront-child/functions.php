4ca20638a99d88693bf51164ee80c8d3118d7c26
Nick Weisser <nick@openstream.ch>
2018-05-11 16:42:52 +0200

    OW-3 Add Openstream credits to child theme and change name space

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -1,7 +1,11 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
+add_action( 'wp_enqueue_scripts', 'openstream_theme_enqueue_styles' );
+function openstream_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
-?>
+
+add_action('storefront_footer', 'openstream_theme_add_footer_credits', 12);
+function openstream_theme_add_footer_credits() {
+    echo '<div>MultilingualPress integration and WooCommerce support by <a href="https://www.openstream.ch">Openstream Internet Solutions</a>.</div>';
+}
